9a581f94db5c4d403063524975ea8b460737baf9
minami <south614fruit@yahoo.co.jp>
2025-10-22 17:31:47 +0900

    task-detail.vueにIDを渡す処理を実装

--- components/TaskList.vue
+++ components/TaskList.vue
@@ -1,21 +1,19 @@
 <script setup>
 import { useTasks } from '~/composables/useTasks';
-import TaskItem from './TaskItem.vue';
 
 const { tasks, loading, error } = useTasks()
 </script>
 
 <template>
-  <div>
-    <h1>Tasks</h1>
-    <div v-if="loading">Loading...</div>
-    <div v-if="error">Error: {{ error }}</div>
+    <div>
+        <h1>Tasks</h1>
+        <div v-if="loading">Loading...</div>
+        <div v-if="error">Error: {{ error }}</div>
 
-    <ul>
-      <TaskItem v-for="task in tasks" :key="task.id" :task="task" />
-    </ul>
-    <NuxtLink to="/tasks/task-detail">
-      <button>TaskDetailに遷移する</button>
-    </NuxtLink>
-  </div>
+        <li v-for="task in tasks" :key="task.id">
+            <NuxtLink :to="`/tasks/detail/${task.id}`">
+                {{ task.title }}
+            </NuxtLink>
+        </li>
+    </div>
 </template>
